5180271ace847669834d89ec18fce6bce63845fc
Michael Schletz <schletz@gmail.com>
2021-10-17 12:22:22 +0200

    Commit 20211017

--- 13_NoSQL/Projekt Pruefungsverwaltung/ExamManager/ExamManager.Application/Documents/IDocument.cs
+++ 13_NoSQL/Projekt Pruefungsverwaltung/ExamManager/ExamManager.Application/Documents/IDocument.cs
@@ -2,6 +2,10 @@
 
 namespace ExamManager.Application.Documents
 {
+    /// <summary>
+    /// Interface für alle Dokumente, die in einer Collection gespeichert werden sollen.
+    /// Definiert einen Key für das generische Repository.
+    /// </summary>
     public interface IDocument<TKey>
     {
         [BsonId]
