ef6af86812f34f31729881d50462fbba92adb536
schletz <schletz@spengergasse.at>
2022-10-15 08:46:29 +0200

    Commit 15/10/2022

---
+++ 13_NoSQL/04_MongoDb_Update/Model/Lehrer.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace MongoDbDemo.Model
+{
+    class Lehrer
+    {
+        public Lehrer(string id, string vorname, string zuname, string? email = null, decimal? gehalt = null)
+        {
+            Id = id;
+            Vorname = vorname;
+            Zuname = zuname;
+            Email = email;
+            Gehalt = gehalt;
+        }
+
+        [BsonId]
+        public string Id { get; private set; }
+        public string Vorname { get; set; }
+        public string Zuname { get; set; }
+        public string? Email { get; set; }
+        public decimal? Gehalt { get; set; }                  // NULLABLE
+        public List<string> Lehrbefaehigungen { get; private set; } = new(0);
+        public override string ToString() => $"     {Id} {Vorname} {Zuname} " +
+            (Gehalt.HasValue ? Gehalt.Value.ToString("0.00") : "(kein Gehalt)") +
+            " Fächer: " + string.Join(", ", Lehrbefaehigungen);
+    }
+}
--- 13_NoSQL/04_MongoDb_Update/Program.cs
+++ 13_NoSQL/04_MongoDb_Update/Program.cs
@@ -3,126 +3,171 @@ using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using Bogus;
+using Bogus.DataSets;
 using Bogus.Extensions;
 using MongoDB.Bson;
 using MongoDB.Driver;
-using MongoDbDemo.Domain;
+using MongoDB.Driver.Core.Events;
+using MongoDbDemo.Model;
 
-namespace MongoDbDemo
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Clear();
+// Verbindet zur Datenbank. Der Container muss laufen und für den User root (Passwort 1234) konfiguriert sein.
+var settings = MongoClientSettings.FromConnectionString("mongodb://root:1234@localhost:27017");
+settings.ServerSelectionTimeout = TimeSpan.FromSeconds(5);
+settings.ClusterConfigurator = cb =>
 {
-    class Program
+    cb.Subscribe<CommandStartedEvent>(e =>
     {
-        static async Task Main(string[] args)
+        if (e.Command.TryGetValue("updates", out var val))
+            Console.WriteLine(val);
+    });
+};
+var client = new MongoClient(settings);
+var db = client.GetDatabase("Stundenplan");
+try
+{
+    SeedDatabase(db);
+}
+catch (TimeoutException)
+{
+    Console.Error.WriteLine("Die Datenbank ist nicht erreichbar. Läuft der Container?");
+    return;
+}
+catch (MongoAuthenticationException)
+{
+    Console.Error.WriteLine("Mit dem angegebenen Benutzer konnte keine Verbindung aufgebaut werden.");
+    return;
+}
+Console.Clear();
+
+// Lehrer ABB ändert die Email Adresse auf abb@spengergasse.at
+PrintHeader("Lehrer ABB ändert die Email Adresse auf abb@spengergasse.at");
+db.GetCollection<Lehrer>(nameof(Lehrer))
+    .UpdateOne(
+        Builders<Lehrer>.Filter.Eq(l => l.Id, "ABB"),
+        Builders<Lehrer>.Update.Set(l => l.Email, "abb@spengergasse.at"
+    ));
+
+// ************************************************************************
+// Lehrer LAN ändert die Email Adresse auf lan@spengergasse.at
+// Beachte: LAN ist auch Klassenvorstand und muss daher in den Klassen
+//          ebenfalls geändert werden
+PrintHeader("Lehrer LAN ändert die Email Adresse auf lan@spengergasse.at");
+db.GetCollection<Lehrer>(nameof(Lehrer))
+    .UpdateOne(
+        Builders<Lehrer>.Filter.Eq(l => l.Id, "LAN"),
+        Builders<Lehrer>.Update.Set(l => l.Email, "lan@spengergasse.at"
+    ));
+db.GetCollection<Klasse>(nameof(Klasse))
+    .UpdateOne(
+        Builders<Klasse>.Filter.Eq(l => l.Kv.Id, "LAN"),
+        Builders<Klasse>.Update.Set(l => l.Kv.Email, "lan@spengergasse.at"
+    ));
+
+// Lehrer ABB bekommt eine zusätzliche Lehrbefähigung  in E
+// Siehe auch https://mongodb.github.io/mongo-csharp-driver/2.5/apidocs/html/Methods_T_MongoDB_Driver_Builders_Update.htm
+PrintHeader("Lehrer ABB bekommt eine zusätzliche Lehrbefähigung  in E");
+db.GetCollection<Lehrer>(nameof(Lehrer))
+    .UpdateOne(
+        Builders<Lehrer>.Filter.Eq(l => l.Id, "ABB"),
+        Builders<Lehrer>.Update.AddToSet(l => l.Lehrbefaehigungen, "E")
+    );
+
+// Einsparung: Alle Lehrer, die mehr als 4000 Euro verdienen, bekommen 100 € weniger
+// Gehalt. Zuerst suchen wir alle Datensätze, dann werden die entsprechenden Update
+// Anweisungen an die Datenbank gesendet.
+PrintHeader("Alle Lehrer, die mehr als 4000 Euro verdienen, bekommen 100 € weniger (mit UpdateOne)");
+db.GetCollection<Lehrer>(nameof(Lehrer))
+    .AsQueryable()
+    .Where(l => l.Gehalt > 4000)
+    .ToList()
+    .ForEach(l =>
+    {
+        db.GetCollection<Lehrer>(nameof(Lehrer)).UpdateOne(
+            Builders<Lehrer>.Filter.Eq(le => le.Id, l.Id),
+            Builders<Lehrer>.Update.Set(le => le.Gehalt, l.Gehalt - 100));
+    });
+
+// Die folgende Lösung verwendet Replace und ersetzt das ganze Lehrerdokument.
+PrintHeader("Alle Lehrer, die mehr als 4000 Euro verdienen, bekommen 100 € weniger (mit ReplaceOne)");
+db.GetCollection<Lehrer>(nameof(Lehrer))
+    .AsQueryable()
+    .Where(l => l.Gehalt > 4000)
+    .ToList()
+    .ForEach(l =>
+    {
+        l.Gehalt = l.Gehalt - 100;
+        db.GetCollection<Lehrer>(nameof(Lehrer)).ReplaceOne(
+            Builders<Lehrer>.Filter.Eq(le => le.Id, l.Id), l);
+    });
+
+// Die folgende Lösung verwendet Bulk Uperationen und ist somit schneller.
+// Eine Projektion projiziert jeden Datensatz auf eine Update Operation
+PrintHeader("Alle Lehrer, die mehr als 4000 Euro verdienen, bekommen 100 € weniger (mit BulkWrite)");
+var requests = db.GetCollection<Lehrer>(nameof(Lehrer))
+    .AsQueryable()
+    .Where(l => l.Gehalt > 4000)
+    .ToList()
+    .Select(l => new UpdateOneModel<Lehrer>(
+        Builders<Lehrer>.Filter.Eq(le => le.Id, l.Id),
+        Builders<Lehrer>.Update.Set(le => le.Gehalt, l.Gehalt - 100)));
+db.GetCollection<Lehrer>(nameof(Lehrer)).BulkWrite(requests);
+
+
+void SeedDatabase(IMongoDatabase db)
+{
+    db.DropCollection(nameof(Lehrer));
+    db.DropCollection(nameof(Klasse));
+
+    Randomizer.Seed = new Random(2112);
+    var faecher = new string[] { "POS", "DBI", "AM", "D", "E", "BWM" };
+    // Wir generieren einige Lehrer mit dem Faker aus dem Paket Bogus.
+    var lehrer = new Faker<Lehrer>("de").CustomInstantiator(f =>
+    {
+        var zuname = f.Name.LastName();
+        var l = new Lehrer(
+            id: zuname.Length < 3 ? zuname.ToUpper() : zuname.Substring(0, 3).ToUpper(),
+            vorname: f.Name.FirstName(),
+            zuname: zuname)
         {
-            var client = new MongoClient("mongodb://127.0.0.1:27017");
-            var db = client.GetDatabase("Stundenplan");
-            SeedDatabase(db);
+            Email = $"{zuname}@spengergasse.at".OrDefault(f, 0.2f),
+            Gehalt = (f.Random.Int(200000, 500000) / 100M).OrNull(f, 0.5f)
+        };
+        // Lehrer sind in 0 - 3 Fächern aus der Liste lehrbefähigt.
+        l.Lehrbefaehigungen.AddRange(f.Random.ListItems(faecher, f.Random.Int(0, 3)));
+        return l;
+    })
+    .Generate(100)
+    .GroupBy(l => l.Id)        // Duplikate vermeiden (gleiche ID darf nicht sein)
+    .Select(l => l.First())
+    .ToList();
 
-            // Lehrer ABB ändert die Email Adresse auf abb@spengergasse.at
-            db.GetCollection<Lehrer>(nameof(Lehrer))
-                .UpdateOne(
-                    Builders<Lehrer>.Filter.Eq(l => l.Id, "ABB"),
-                    Builders<Lehrer>.Update.Set(l => l.Email, "abb@spengergasse.at"
-                ));
+    db.GetCollection<Lehrer>(nameof(Lehrer)).InsertMany(lehrer);
 
-            // ************************************************************************
-            // Lehrer LAN ändert die Email Adresse auf lan@spengergasse.at
-            // Beachte: LAN ist auch Klassenvorstand und muss daher in den Klassen
-            //          ebenfalls geändert werden
-            db.GetCollection<Lehrer>(nameof(Lehrer))
-                .UpdateOne(
-                    Builders<Lehrer>.Filter.Eq(l => l.Id, "LAN"),
-                    Builders<Lehrer>.Update.Set(l => l.Email, "lan@spengergasse.at"
-                ));
-            db.GetCollection<Klasse>(nameof(Klasse))
-                .UpdateOne(
-                    Builders<Klasse>.Filter.Eq(l => l.Kv.Id, "LAN"),
-                    Builders<Klasse>.Update.Set(l => l.Kv.Email, "lan@spengergasse.at"
-                ));
+    var abteilungen = new string[] { "HIF", "AIF", "BIF" };
+    // Wir generieren einige Klassen.
+    var klassen = new Faker<Klasse>("de").CustomInstantiator(f =>
+    {
+        return new Klasse(
+            id: f.Random.Int(1, 5) + f.Random.String2(1, "ABCD") + f.Random.ListItem(abteilungen),
+            kv: f.Random.ListItem(lehrer));
+    })
+    .Generate(100)
+    .GroupBy(k => k.Id)       // Duplikate vermeiden (gleiche ID darf nicht sein)
+    .Select(k => k.First())
+    .Take(10)
+    .ToList();
 
-            // Lehrer ABB bekommt eine zusätzliche Lehrbefähigung  in E
-            // Siehe auch https://mongodb.github.io/mongo-csharp-driver/2.5/apidocs/html/Methods_T_MongoDB_Driver_Builders_Update.htm
-            db.GetCollection<Lehrer>(nameof(Lehrer))
-                .UpdateOne(
-                    Builders<Lehrer>.Filter.Eq(l => l.Id, "ABB"),
-                    Builders<Lehrer>.Update.AddToSet(l => l.Lehrbefaehigung, "E")
-                );
+    db.GetCollection<Klasse>(nameof(Klasse)).InsertMany(klassen);
+}
 
-            // Einsparung: Alle Lehrer, die mehr als 4000 Euro verdienen, bekommen 100 € weniger
-            // Gehalt. Zuerst suchen wir alle Datensätze, dann werden die entsprechenden Update
-            // Anweisungen an die Datenbank gesendet.
-            db.GetCollection<Lehrer>(nameof(Lehrer))
-                .AsQueryable()
-                .Where(l => l.Gehalt > 4000)
-                .ToList()
-                .ForEach(l =>
-                {
-                    db.GetCollection<Lehrer>(nameof(Lehrer)).UpdateOne(
-                        Builders<Lehrer>.Filter.Eq(le => le.Id, l.Id),
-                        Builders<Lehrer>.Update.Set(le => le.Gehalt, l.Gehalt - 100));
-                });
-
-            // Die folgende Lösung verwendet Replace und ersetzt das ganze Lehrerdokument.
-            db.GetCollection<Lehrer>(nameof(Lehrer))
-                .AsQueryable()
-                .Where(l => l.Gehalt > 4000)
-                .ToList()
-                .ForEach(l =>
-                {
-                    l.Gehalt = l.Gehalt - 100;
-                    db.GetCollection<Lehrer>(nameof(Lehrer)).ReplaceOne(
-                        Builders<Lehrer>.Filter.Eq(le => le.Id, l.Id), l);
-                });
-
-            // Die folgende Lösung verwendet Bulk Uperationen und ist somit schneller.
-            // Eine Projektion projiziert jeden Datensatz auf eine Update Operation
-            var requests = db.GetCollection<Lehrer>(nameof(Lehrer))
-                .AsQueryable()
-                .Where(l => l.Gehalt > 4000)
-                .ToList()
-                .Select(l => new UpdateOneModel<Lehrer>(
-                    Builders<Lehrer>.Filter.Eq(le => le.Id, l.Id),
-                    Builders<Lehrer>.Update.Set(le => le.Gehalt, l.Gehalt - 100)));
-            db.GetCollection<Lehrer>(nameof(Lehrer)).BulkWrite(requests);
-
-
-        }
-        private static void SeedDatabase(IMongoDatabase db)
-        {
-            db.DropCollection(nameof(Lehrer));
-            db.DropCollection(nameof(Klasse));
-
-            Randomizer.Seed = new Random(2112);
-            var faecher = new string[] { "POS", "DBI", "AM", "D", "E", "BWM" };
-            var lehrer = new Faker<Lehrer>().Rules((f, l) =>
-            {
-                l.Vorname = f.Name.FirstName();
-                l.Zuname = f.Name.LastName();
-                l.Id = l.Zuname.Substring(0, 3).ToUpper();
-                l.Email = $"{l.Zuname}@spengergasse.at".OrDefault(f, 0.2f);
-                // Gehalt
-                l.Gehalt = f.Random.Decimal2(2000, 5000).OrNull(f, 0.5f);
-                // Lehrer sind in 0 - 3 Fächern aus der Liste lehrbefähigt.
-                l.Lehrbefaehigung = f.Random.ListItems(faecher, f.Random.Int(0, 3)).ToArray();
-            })
-                .Generate(100)
-                .ToHashSet()
-                .ToList();
-
-            db.GetCollection<Lehrer>(nameof(Lehrer)).InsertMany(lehrer);
-
-            var abteilungen = new string[] { "HIF", "AIF", "BIF" };
-
-            var klassen = new Faker<Klasse>().Rules((f, k) =>
-            {
-                k.Id = f.Random.Int(1, 5) + f.Random.String2(1, "ABCD") + f.Random.ListItem(abteilungen);
-                k.Kv = f.Random.ListItem(lehrer);
-            })
-                .Generate(10)
-                .ToHashSet()
-                .ToList();
-
-            db.GetCollection<Klasse>(nameof(Klasse)).InsertMany(klassen);
-        }
-    }
+void PrintHeader(string text)
+{
+    var color = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(text);
+    Console.ForegroundColor = color;
 }
